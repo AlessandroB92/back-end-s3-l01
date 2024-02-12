fe2e70ee4ea422290208a64c1d1fbc498dabce35
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-02-12 15:53:44 +0100

    first commit

---
+++ ASP.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="back_end_s3_l01.Global" Language="C#" %>
---
+++ ASP.NET/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace back_end_s3_l01
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            FirstName.Text = ConfigurationManager.AppSettings["FirstName"];
+            LastName.Text = ConfigurationManager.AppSettings["LastName"];
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
